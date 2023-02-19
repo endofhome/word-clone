420e4adfdbda1dd68afd4758aab1f21e6a0a9bbb
Tom Barnsbury <ruralmyth@gmail.com>
2023-02-19 21:48:03 +0000

    Exercise 3 complete.
    I'm not sure if this is overcomplicated, but I don't understand Wordle very well.

--- src/components/GuessHistory/GuessHistory.js
+++ src/components/GuessHistory/GuessHistory.js
@@ -1,19 +1,43 @@
 import React from "react";
+import {range} from "../../utils";
+import {NUM_OF_GUESSES_ALLOWED} from "../../constants";
 
 function GuessHistory({ guessHistory }) {
-  return <div
-      className="guess-history"
-      style={{
-        textTransform: "uppercase"
-      }}
+    const emptyGuess = range(0, 5).map(() => '');
+    const maximumEmptyGuesses = range(0, NUM_OF_GUESSES_ALLOWED).map(() => emptyGuess);
+
+    function lastSixItems(_, i) {
+        return i >= (guessHistory.length - NUM_OF_GUESSES_ALLOWED);
+    }
+
+    function firstSixItems(_, i) {
+        return i < 6;
+    }
+
+    return <div
+        className="guess-history"
+        style={{
+            textTransform: "uppercase"
+        }}
     >
-    {guessHistory.map(historyItem =>
-      <p
-          className="guess"
-          key={crypto.randomUUID()}
-      >{historyItem}</p>
-    )}
-  </div>;
+        {guessHistory
+            .filter(lastSixItems)
+            .concat(maximumEmptyGuesses)
+            .filter(firstSixItems)
+            .map(historyItem =>
+                <p
+                    className="guess"
+                    key={crypto.randomUUID()}>
+                    {range(0, historyItem.length)
+                        .map(i =>
+                            <span
+                                className="cell"
+                                key={i}
+                            >{historyItem[i]}</span>)}
+                </p>
+            )
+        }
+    </div>;
 }
 
 export default GuessHistory;
